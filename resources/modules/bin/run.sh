389a2320ff90bef29e37923ea9d602d244e5c42d
yuzor <kenneth@cyamast.com>
2023-01-23 16:49:24 +1100

    db migration in progress

--- resources/modules/bin/run.sh
+++ resources/modules/bin/run.sh
@@ -1,1 +1,1 @@
-java -Domnisearch.config.dir.path="../conf" -Domnisearch.home="../" -cp "../lib/*" org.omnisearch.framework.ProductCore
+java -Domnisearch.config.dir.path="../conf" -Domnisearch.home="../" -Domnisearch.resources.dir.path="../resources" -cp "../lib/*" org.omnisearch.framework.ProductCore
